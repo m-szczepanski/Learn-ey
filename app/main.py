91bba46fcae76119278b03b6f128492fe9252970
Matthew <30538559+m-szczepanski@users.noreply.github.com>
2023-10-09 12:49:04 +0200

    implementation of main panel ui

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 import tkinter as tk
 from components.main_panel import MainPanel
 
+
+def quit_app():
+    root.destroy()
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = MainPanel(root)
